4363f947823f8b74542ade4f83c92bc822b5e0d1
Sir-Phil <52827424+Sir-Phil@users.noreply.github.com>
2023-06-25 17:30:34 +0100

    activity routes

--- src/routes/touractivitiesRoute.ts
+++ src/routes/touractivitiesRoute.ts
@@ -6,6 +6,6 @@ const router = express.Router();
 
 router.route("/allActivities").get(getAll);
 router.route("/SearchTour").get(searchActivities);
-router.route('/TopTours').get(getTopTourByReview)
+router.route("/TopTours").get(getTopTourByReview)
 
 export default router;
